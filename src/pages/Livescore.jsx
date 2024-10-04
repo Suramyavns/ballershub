6d456fadb3a40f00fbaf3feb206daefdea82d1c7
suramyavns <sdidwania645@gmail.com>
2024-10-05 01:04:38 +0530

    seo

--- src/pages/Livescore.jsx
+++ src/pages/Livescore.jsx
@@ -3,6 +3,7 @@ const Livescore = () => {
   const scoreBat_API = import.meta.env.VITE_SCOREBAT_TOKEN;
   return (
     <iframe
+    title="Live Feed"
     src={`https://www.scorebat.com/embed/livescore/?token=${scoreBat_API}`}
     allow="autoplay"
     style={{ width: "100%", height: '100vh', display: "block" }}
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -2,6 +2,7 @@ const Video = () => {
   const scoreBat_API = import.meta.env.VITE_SCOREBAT_TOKEN;
   return (
     <iframe
+    title="Video Feed"
         src={`https://www.scorebat.com/embed/videofeed/?token=${scoreBat_API}`}
         allow="autoplay"
         style={{ width: "100%", height: '100vh', overflow: "hidden", display: "block" }}
